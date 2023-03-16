b85aa266533d8f2f3719768297b8be460152c61e
Alex <sasha8562@gmail.com>
2023-03-17 01:37:01 +0300

    start

---
+++ Source/Test_1/Test_1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Test_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test_1, "Test_1" );
